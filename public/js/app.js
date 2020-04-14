9faeb53dee0b9217aed10146124d526f2337b075
Sourav Choudhary <rinkeshchoudhary3@gmail.com>
2020-04-14 17:49:29 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
@@ -23,7 +21,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading.... '
     messageTwo.textContent = ''
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
